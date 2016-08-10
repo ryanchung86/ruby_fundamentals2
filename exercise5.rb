9b8c30fa8f558e526c6df587bf47d8ddb43677b6
Ryan <ryan.y.chung@gmail.com>
2016-08-10 15:00:17 -0400

    changed code block to include string interpolation

--- exercise5.rb
+++ exercise5.rb
@@ -2,8 +2,7 @@ puts "I prompt thee for a temperature in Fahrenheit"
 temp = gets.chomp
 
 def temp_in_f(temp)
-  (temp - 32) * 5 / 9
-#  p "Celsius is better. Your lame temp is actually #{temp} Celsius"
+  puts "Celsius is better. Your lame temperature is actually #{(temp.to_i - 32) * 5 / 9} degrees Celsius"
 end
 
 temp_in_f(72)
