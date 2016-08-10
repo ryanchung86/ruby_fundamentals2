751804d7f8cbeb5345c74c393b61f3a2f0fa6b95
Ryan <ryan.y.chung@gmail.com>
2016-08-10 15:19:49 -0400

    completed exercise5

--- exercise5.rb
+++ exercise5.rb
@@ -1,8 +1,8 @@
 puts "I prompt thee for a temperature in Fahrenheit"
-temp = gets.chomp
+temp = gets
 
-def temp_in_f(temp)
-  puts "Celsius is better. Your lame temperature is actually #{(temp.to_i - 32) * 5 / 9} degrees Celsius"
+def temp_in_f(temp2)
+  p "Celsius is better. Your lame temperature is actually #{(temp2.to_i - 32) * 5 / 9} degrees Celsius"
 end
 
-temp_in_f(72)
+temp_in_f(temp)
