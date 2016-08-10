016366e9f7f786affc131f276ab1d52e8e100c51
Ryan <ryan.y.chung@gmail.com>
2016-08-10 14:55:29 -0400

    checking to see if method works on its own

--- exercise5.rb
+++ exercise5.rb
@@ -1,7 +1,9 @@
 puts "I prompt thee for a temperature in Fahrenheit"
+temp = gets.chomp
+
 def temp_in_f(temp)
   (temp - 32) * 5 / 9
-  p "Celsius is better. Your lame temp is actually #{temp} Celsius"
+#  p "Celsius is better. Your lame temp is actually #{temp} Celsius"
 end
 
 temp_in_f(72)
