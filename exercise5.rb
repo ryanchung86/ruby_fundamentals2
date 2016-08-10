75878dceb55e58a9fb60f1efb095ca631f6f1979
Ryan <ryan.y.chung@gmail.com>
2016-08-10 14:43:03 -0400

    first attempt at exercise 5 which is wrong

---
+++ exercise5.rb
@@ -0,0 +1,7 @@
+puts "I prompt thee for a temperature in Fahrenheit"
+def temp_in_f(temp)
+  (temp - 32) * 5 / 9
+  p "Celsius is better. Your lame temp is actually #{temp} Celsius"
+end
+
+temp_in_f(72)
