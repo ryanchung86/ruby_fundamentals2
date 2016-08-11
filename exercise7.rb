dc484d1fb39fe731a7a0cefe8fcd9fd118b27cb0
Ryan <ryan.y.chung@gmail.com>
2016-08-10 21:27:21 -0400

    added method to display cohorts and student numbers

---
+++ exercise7.rb
@@ -0,0 +1,13 @@
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+def cohorts(students)
+  students.each do |cohort, num|
+    puts "#{cohort}: #{num} students"
+  end
+end
+
+cohorts(students)
